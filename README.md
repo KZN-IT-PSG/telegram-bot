0424c42d5152d0113f99bef326ce872c34a6b704
DamirF <d.fatkhullin@mail.ru>
2022-07-31 14:26:40 +0300

    Iint commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Telegram-bot for determine the type of bearing.
+
+Necessary packages for the bot to work:
+aiogram;
+tensorflow;
+opencv-python.
+
+Setting up the environment for the bot:
+Use folowing commands:
+python -m venv (your venv name)
+(your venv name)\Scripts\activate
+pip install --ignore-installed aiogram
+pip install --ignore-installed tensorflow-gpu (or tensorflow-cpu)
+pip install --ignore-installed opencv-python
+pip install --ignore-installed sklearn
+
+Done!
+
+For launch the bot you can create .bat file (for Windows)
+
+.bat file content:
+
+@echo off
+call %~dp0(your venv name)\Scripts\activate
+set TOKEN=(your bot token)
+python bot-main.py
+pause
---
+++ bot-main.py
@@ -0,0 +1,62 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from keras.models import load_model
+import pickle
+
+import os
+import cv2
+
+IMAGE_SIZE = 32
+PATH = 'img/am.jpg'
+bot = Bot(os.environ.get('TOKEN'))
+dp = Dispatcher(bot)
+
+
+names = ['Подшипник-3612 (Роликовый подшипник).', 'Игольчатый подшибник.', 'Шариковый подшипник.']
+
+
+def predict(path):
+    image = cv2.imread(path)
+    output = image.copy()
+    image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+
+    image = image.astype("float") / 255.0
+
+    if 1:
+        image = image.flatten()
+        image = image.reshape((1, image.shape[0]))
+
+    else:
+        image = image.reshape((1, image.shape[0], image.shape[1],
+                               image.shape[2]))
+
+    print("[INFO] loading network and label binarizer...")
+    model = load_model("model")
+    lb = pickle.loads(open("label.bin", "rb").read())
+
+    preds = model.predict(image)
+
+    i = preds.argmax(axis=1)[0]
+    label = names[i]
+    return label
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def start_message(message):
+    await bot.send_message(message.from_user.id, 'Отправь мне фото подшипника и я определю его тип.')
+
+
+@dp.message_handler(content_types=['photo'])
+async def photo_predict(message):
+    await bot.send_message(message.from_user.id, 'Фото обрабатывается...')
+    await message.photo[-1].download(PATH)
+    await message.reply(predict(PATH))
+
+
+def main():
+    executor.start_polling(dp, skip_updates=True)
+
+
+if __name__ == '__main__':
+    main()
